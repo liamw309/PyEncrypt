94880fe294d338a03dba2785c6f878f96caeb831
liamw309 <liamw309@me.com>
2015-02-20 12:09:12 -0500

    Updated Beta V1.1

--- encrypt.py
+++ encrypt.py
@@ -6,21 +6,30 @@ class encrypt(object):
 	This class is used to actually encrypt the string
 	'''
 	def __init__(self):
+		'''
+		This method is used to initialize the class.
+		Attributes: initial (what the user wants encrypted), new (the string after it is encrypted).
+		'''
 		self.initial = []
 		self.new = ''
 	def getstr(self):
+		'''
+		This method gets what the user wants to encrypt.
+		Attributes: initial (what the user wants encrypted).
+		'''
 		self.initial = raw_input('What would you like to encrypt? ')
 	def encrypt(self):
+		'''
+		This method takes the string that the user wants encrypted and encrypts it with a for loop.
+		Attributes: alphabet (list of characters), key (key), new (encrypted string).
+		'''
 		alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 		key = k.key
 		self.new = self.initial.lower()
 		for x in range(0,35):
 			self.new.replace(alphabet[x],key[x])
-	def getencrypt(self):
-		return self.new, self.initial
 
 a = encrypt()
 a.getstr()
 a.encrypt()
-a.getencrypt()
 print(a.new)
