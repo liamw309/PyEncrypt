c09d32780c9cad5678e8740ff8da411413608652
liamw309 <liamw309@me.com>
2015-02-20 11:52:47 -0500

    Updated Beta V1

--- encrypt.py
+++ encrypt.py
@@ -5,8 +5,6 @@ class encrypt(object):
 	'''
 	This class is used to actually encrypt the string
 	'''
-	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-	key = k.key
 	def __init__(self):
 		self.initial = []
 		self.new = ''
