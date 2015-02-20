2152f9355869e6ade3d1ca46e26ccdfc520336f6
liamw309 <liamw309@me.com>
2015-02-20 11:46:50 -0500

    Updated Beta V1

--- encrypt.py
+++ encrypt.py
@@ -1,3 +1,28 @@
 import keycreater as k
 k = k.keycreater()
-print(k.createkey())
+print(k.key)
+class encrypt(object):
+	'''
+	This class is used to actually encrypt the string
+	'''
+	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+	key = k.key
+	def __init__(self):
+		self.initial = []
+		self.new = ''
+	def getstr(self):
+		self.initial = raw_input('What would you like to encrypt? ')
+	def encrypt(self):
+		alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+		key = k.key
+		self.new = self.initial.lower()
+		for x in range(0,35):
+			self.new.replace(alphabet[x],key[x])
+	def getencrypt(self):
+		return self.new, self.initial
+
+a = encrypt()
+a.getstr()
+a.encrypt()
+a.getencrypt()
+print(a.new)
--- keycreater.py
+++ keycreater.py
@@ -1,7 +1,8 @@
 import random
-
 class keycreater(object):
-
+    '''
+    This class is used to create a key for the encryption
+    '''
     def __init__(self):
         '''
         This method helps to create the key. It creates a whole new key by appending a new list (key) and removing it from the old list (alphabet). It does this in a random order.  Attributes: alphabet (base list), key (new list), char (random letter/number from alphabet)
@@ -14,6 +15,5 @@ class keycreater(object):
         	char = random.choice(alphabet)
 	        alphabet.remove(char)
 	        self.key.append(char)
-
     def createkey(self):
         return self.key
